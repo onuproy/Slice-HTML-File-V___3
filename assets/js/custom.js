8038426df147dd2bf56fe980aae586f28327a5ee
Onup Chandra Barmon <43741354+onuproy@users.noreply.github.com>
2022-08-19 17:38:28 +0600

    update last v 4 all files

---
+++ assets/js/custom.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function(){ 
+
+	 
+
+
+	 // DropDown Menu 
+    jQuery('.dropdown_menu a').click(function () {
+        jQuery('.dropdown-content').slideToggle(800);
+         return false;
+    });
+     // DropDown Menu 2
+    jQuery('.dropdown_menu2 a').click(function () {
+        jQuery('.dropdown-content2').slideToggle(800);
+         return false;
+    });
+      // DropDown Menu 3
+    jQuery('.dropdown_menu3 a').click(function () {
+        jQuery('.dropdown-content3').slideToggle(800);
+         return false;
+    });
+
+		
+		
+	
+
+});
